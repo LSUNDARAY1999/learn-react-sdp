007a5a62c2c2c7ff359519f59d385e8b1b914f7b
Ananya Bisoi <aanyabisoi996@gmail.com>
2026-01-11 21:03:19 +0530

    react router

---
+++ AB_07/day4/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+      <div className="bg-white p-6 rounded-lg shadow">
+      <h1 className="text-2xl font-bold mb-2">Home Page</h1>
+      <p className="text-gray-600">Welcome to the Home page.</p>
+    </div>
+  )
+}
+
+export default Home
